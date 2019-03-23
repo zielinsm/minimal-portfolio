7fbd606e6c44f83fd790f56d7aaef67f23e8e3a3
Michał Zieliński <michal@bejamas.io>
2019-03-23 09:51:36 +0100

    Debug the CircleCI configuration

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -8,4 +8,4 @@ git config user.email "zielinski-michal@outlook.com"
 git add .
 git commit -m "Deploy from CircleCI"
 
-git push --force --quiet "https://${GH_TOKEN}@github.com/zielinsm/minimal-portfolio.git" master:gh-pagesn
+git push --force --quiet "https://${GH_TOKEN}@github.com/zielinsm/minimal-portfolio.git" master:gh-pages
